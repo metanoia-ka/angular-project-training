3ddac38071916bea85085f5aa50144701a03a849
Norbert <norbetltagne@gmail.com>
2025-04-11 13:16:40 +0100

    add commentaries

--- src/app/components/mon-composant/mon-composant.component.ts
+++ src/app/components/mon-composant/mon-composant.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-
+// Le décorateur @Component permet de définir un composant Angular
+// Le sélecteur 'app-mon-composant' est utilisé pour référencer ce composant dans d'autres templates
+// imports : On peut importer d'autres composants, directives ou pipes ici si nécessaire
+// templateUrl : Chemin vers le fichier HTML du composant
 @Component({
   selector: 'app-mon-composant',
   imports: [],
